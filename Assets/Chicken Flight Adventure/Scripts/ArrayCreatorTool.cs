6c2b32e03663181caa9ec94aaf2b3d36c75bbd46
PhatPingu <85648352+PhatPingu@users.noreply.github.com>
2022-05-19 13:19:30 -0300

    Array Creator Tool agora atualiza as Coords em Editor

--- Assets/Chicken Flight Adventure/Scripts/ArrayCreatorTool.cs
+++ Assets/Chicken Flight Adventure/Scripts/ArrayCreatorTool.cs
@@ -10,28 +10,47 @@ public struct ArrayTool
     public float zPosMin, zPosMax;
 }
 
+[System.Serializable]
+public struct Variation
+{
+    public float xVariation;
+    public float zVariation;
+}
 
 
+[ExecuteInEditMode]
 public class ArrayCreatorTool : MonoBehaviour
 {
+    [ContextMenu("Do Something")]
+    void DoSomething()
+    {
+        Debug.Log("Perform operation");
+    }
+    
+    [Header("### Variation ###")]
+    [SerializeField] private Variation[] variation_line_01;
+    [SerializeField] private Variation[] variation_line_02;
+    [SerializeField] private Variation[] variation_line_03;
+    [SerializeField] private Variation[] variation_line_04;
 
-    [Header("Coords")]
+
+    [Header("### Coords ###")]
     [SerializeField] private ArrayTool[] line_01;
     [SerializeField] private ArrayTool[] line_02;
     [SerializeField] private ArrayTool[] line_03;
     [SerializeField] private ArrayTool[] line_04;
 
 
-    [Header("Objects")]
+    [Header("### Objects ###")]
     [SerializeField] private GameObject[] object_line_01;
     [SerializeField] private GameObject[] object_line_02;
     [SerializeField] private GameObject[] object_line_03;
     [SerializeField] private GameObject[] object_line_04;
 
-    void Start()
+    void Update()
     {
         UpdateObjectList();
-
+        UpdateCoordinates();
     }
 
     void UpdateObjectList()
@@ -61,14 +80,50 @@ public class ArrayCreatorTool : MonoBehaviour
     {
         for (int i = 0; i < line_01.Length; i++)
         {
-            line_01[i].xPosMin = object_line_01[i].transform.position.x;
-            line_01[i].xPosMax = object_line_01[i].transform.position.x;
+            line_01[i].xPosMin = object_line_01[i].transform.position.x - variation_line_01[i].xVariation;
+            line_01[i].xPosMax = object_line_01[i].transform.position.x + variation_line_01[i].xVariation;
             line_01[i].yPos = object_line_01[i].transform.position.y;
-            line_01[i].yPosOffset = object_line_01[i].transform.position.y;
-            line_01[i].zPosMin = object_line_01[i].transform.position.z;
-            line_01[i].zPosMax = object_line_01[i].transform.position.z;
+            line_01[i].yPosOffset = 0f;
+            line_01[i].zPosMin = object_line_01[i].transform.position.z - variation_line_01[i].zVariation;
+            line_01[i].zPosMax = object_line_01[i].transform.position.z + variation_line_01[i].zVariation;
         }
+
+        for (int i = 0; i < line_02.Length; i++)
+        {
+            line_02[i].xPosMin = object_line_02[i].transform.position.x - variation_line_02[i].xVariation;
+            line_02[i].xPosMax = object_line_02[i].transform.position.x + variation_line_02[i].xVariation;
+            line_02[i].yPos = object_line_02[i].transform.position.y;
+            line_02[i].yPosOffset = 0f;
+            line_02[i].zPosMin = object_line_02[i].transform.position.z - variation_line_02[i].zVariation;
+            line_02[i].zPosMax = object_line_02[i].transform.position.z + variation_line_02[i].zVariation;
+        }
+
+        for (int i = 0; i < line_03.Length; i++)
+        {
+            line_03[i].xPosMin = object_line_03[i].transform.position.x - variation_line_03[i].xVariation;
+            line_03[i].xPosMax = object_line_03[i].transform.position.x + variation_line_03[i].xVariation;
+            line_03[i].yPos = object_line_03[i].transform.position.y;
+            line_03[i].yPosOffset = 0f;
+            line_03[i].zPosMin = object_line_03[i].transform.position.z - variation_line_03[i].zVariation;
+            line_03[i].zPosMax = object_line_03[i].transform.position.z + variation_line_03[i].zVariation;
+        }
+
+        for (int i = 0; i < line_04.Length; i++)
+        {
+            line_04[i].xPosMin = object_line_04[i].transform.position.x - variation_line_04[i].xVariation;
+            line_04[i].xPosMax = object_line_04[i].transform.position.x + variation_line_04[i].xVariation;
+            line_04[i].yPos = object_line_04[i].transform.position.y;
+            line_04[i].yPosOffset = 0f;
+            line_04[i].zPosMin = object_line_04[i].transform.position.z - variation_line_04[i].zVariation;
+            line_04[i].zPosMax = object_line_04[i].transform.position.z + variation_line_04[i].zVariation;
+        }
+
     }
 
+    [ContextMenu("Create Spawner")]
+    void CreateSpawner()
+    {
+        Debug.Log("Creating Spawner");
+    }
 
 }
