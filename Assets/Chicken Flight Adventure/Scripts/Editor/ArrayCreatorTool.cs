78128148b88a384238345f2165acea9dc65e145c
PhatPingu <85648352+PhatPingu@users.noreply.github.com>
2022-05-29 20:26:33 -0300

    Ajuste de Editor para não interferir na BUILD

--- Assets/Chicken Flight Adventure/Scripts/Editor/ArrayCreatorTool.cs
+++ Assets/Chicken Flight Adventure/Scripts/Editor/ArrayCreatorTool.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using UnityEditor.SceneManagement;
 
 [System.Serializable]
 public struct ArrayTool
