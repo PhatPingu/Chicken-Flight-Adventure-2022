ac8257d1be0001076a4c8d0af9c97db825d43597
PhatPingu <85648352+PhatPingu@users.noreply.github.com>
2022-05-29 20:46:16 -0300

    First Commit

---
+++ Assets/Chicken Flight Adventure/Scripts/i_Editor/ArrayCreator_CustomInspector.cs
@@ -0,0 +1,20 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ArrayCreatorTool))]
+public class ArrayCreator_CustomInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ArrayCreatorTool arrayCreatorTool = (ArrayCreatorTool)target;
+        if(GUILayout.Button("Create Spawner with Coords"))
+        {
+            arrayCreatorTool.CreateSpawner();
+        }
+    }  
+    
+}
+#endif
