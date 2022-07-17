1e09f799714142e59671f990c30d6520d5dc69d8
PhatPingu <85648352+PhatPingu@users.noreply.github.com>
2022-07-17 13:59:23 -0300

    Improve Diving

--- Assets/Chicken Flight Adventure/Scripts/Camera/CameraWindEffect.cs
+++ Assets/Chicken Flight Adventure/Scripts/Camera/CameraWindEffect.cs
@@ -13,6 +13,10 @@ public class CameraWindEffect : MonoBehaviour
     [SerializeField] GameObject topLeftCorner;
     [SerializeField] GameObject bottomRightCorner;
     [SerializeField] GameObject bottomLeftCorner;
+    [SerializeField] GameObject top;
+    [SerializeField] GameObject bottom;
+    [SerializeField] GameObject right;
+    [SerializeField] GameObject left;
 
     
     [Header("Camera References")]
@@ -23,6 +27,10 @@ public class CameraWindEffect : MonoBehaviour
     private LineRenderer lineRenderer_02;
     private LineRenderer lineRenderer_03;
     private LineRenderer lineRenderer_04;
+    private LineRenderer lineRenderer_05;
+    private LineRenderer lineRenderer_06;
+    private LineRenderer lineRenderer_07;
+    private LineRenderer lineRenderer_08;
 
     void Start()
     {
@@ -30,6 +38,10 @@ public class CameraWindEffect : MonoBehaviour
         lineRenderer_02 = topLeftCorner.GetComponent<LineRenderer>();
         lineRenderer_03 = bottomRightCorner.GetComponent<LineRenderer>();
         lineRenderer_04 = bottomLeftCorner.GetComponent<LineRenderer>();
+        lineRenderer_05 = top.GetComponent<LineRenderer>();
+        lineRenderer_06 = bottom.GetComponent<LineRenderer>();
+        lineRenderer_07 = right.GetComponent<LineRenderer>();
+        lineRenderer_08 = left.GetComponent<LineRenderer>();
     }
 
     void Update()
@@ -43,6 +55,11 @@ public class CameraWindEffect : MonoBehaviour
         lineRenderer_02.enabled = _playerBehaviour.inputDive && _playerBehaviour.EndDiveBoost_enabled;       
         lineRenderer_03.enabled = _playerBehaviour.inputDive && _playerBehaviour.EndDiveBoost_enabled;
         lineRenderer_04.enabled = _playerBehaviour.inputDive && _playerBehaviour.EndDiveBoost_enabled;
+        lineRenderer_05.enabled = _playerBehaviour.inputDive && _playerBehaviour.EndDiveBoost_enabled;
+        lineRenderer_06.enabled = _playerBehaviour.inputDive && _playerBehaviour.EndDiveBoost_enabled;       
+        lineRenderer_07.enabled = _playerBehaviour.inputDive && _playerBehaviour.EndDiveBoost_enabled;
+        lineRenderer_08.enabled = _playerBehaviour.inputDive && _playerBehaviour.EndDiveBoost_enabled;
+
         _cameraBehaviour.MoveCameraUp(_playerBehaviour.inputDive, cameraMoveUpSpeed);
 
         if(_playerBehaviour.inputDive && _playerBehaviour.EndDiveBoost_enabled)
@@ -57,7 +74,19 @@ public class CameraWindEffect : MonoBehaviour
             lineRenderer_03.SetPosition(1, bottomRightCorner.transform.position);
 
             lineRenderer_04.SetPosition(0, player.transform.position);
-            lineRenderer_04.SetPosition(1, bottomLeftCorner.transform.position);       
+            lineRenderer_04.SetPosition(1, bottomLeftCorner.transform.position);     
+
+            lineRenderer_05.SetPosition(0, player.transform.position);
+            lineRenderer_05.SetPosition(1, top.transform.position);
+        
+            lineRenderer_06.SetPosition(0, player.transform.position);
+            lineRenderer_06.SetPosition(1, bottom.transform.position);
+
+            lineRenderer_07.SetPosition(0, player.transform.position);
+            lineRenderer_07.SetPosition(1, right.transform.position);
+
+            lineRenderer_08.SetPosition(0, player.transform.position);
+            lineRenderer_08.SetPosition(1, left.transform.position);      
         }
     }
 }
